1c228ac1df1bc8e45489a4860f7589cec00e3c25
Antonio Hauren <antoniohauren@protonmail.com>
2024-04-04 15:52:18 -0300

    chore: index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello rosetta")
+import { RosettaRepository } from "./repository/rosetta-repository";
+import { FileService } from "./service/file-service";
+import { RosettaService } from "./service/rosetta-service";
+
+const repository = new RosettaRepository();
+const fileService = new FileService();
+const service = new RosettaService(repository, fileService);
+
+(async () => {
+  // await service.addAllRosettasFromFile();
+  // console.log(await service.getMissingRosettasByLang("pt_PT"));
+
+  // await service.updateRosetta("NOVO", "chave", { pt_PT: "valor safadinho" })
+  // console.log(await service.getMissingRosettasByLang("pt_PT"));
+
+  await service.writeFileByLang("pt_PT");
+
+  repository.done();
+})();
