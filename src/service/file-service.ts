2289eeb6597faf79a5495423ed96fe6ec1a07e68
Antonio Hauren <antoniohauren@protonmail.com>
2024-04-04 15:01:07 -0300

    chore: read file

--- src/service/file-service.ts
+++ src/service/file-service.ts
@@ -2,6 +2,13 @@ import fs from "node:fs";
 import type { RosettaKeys, SerializedRosetta } from "../models/rosetta";
 
 export class FileService {
+
+  readFile(lang: RosettaKeys) {
+    const input = "./input";
+
+    return fs.readFileSync(`${input}/${lang}.json`).toString();
+  }
+
   writeFile(lang: RosettaKeys, data: SerializedRosetta) {
     const output = "./output";
 
@@ -9,6 +16,6 @@ export class FileService {
       fs.mkdirSync(output);
     }
 
-    fs.writeFileSync(`./${output}/${lang}.json`, JSON.stringify(data, null, 2));
+    fs.writeFileSync(`${output}/${lang}.json`, JSON.stringify(data, null, 2));
   }
 }
