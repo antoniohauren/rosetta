232c1070b0243f71022ebbbf2435a8f685298b9d
Antonio Hauren <antoniohauren@protonmail.com>
2024-04-04 13:35:43 -0300

    style: fix lint

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,12 +1,12 @@
 import m from "mongoose";
 
 const rosettaSchema = new m.Schema({
-	key: { type: String, required: true },
-	category: { type: String, required: true },
-	pt_BR: { type: String },
-	en_US: { type: String },
-	pt_PT: { type: String },
-	es_ES: { type: String },
+  key: { type: String, required: true },
+  category: { type: String, required: true },
+  pt_BR: { type: String },
+  en_US: { type: String },
+  pt_PT: { type: String },
+  es_ES: { type: String },
 });
 
 rosettaSchema.index({ key: 1, category: 1 }, { unique: true });
