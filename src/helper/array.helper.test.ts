5d1b23c313c65581dc07437c8c13218210a15abc
Antonio Hauren <antoniohauren@protonmail.com>
2024-04-05 12:06:47 -0300

    test: add case with multi level tree like array

--- src/helper/array.helper.test.ts
+++ src/helper/array.helper.test.ts
@@ -18,6 +18,36 @@ describe("array-helper", () => {
     assert.deepEqual(res, exp);
   });
 
+  it("should flatten tree like array", () => {
+    const input: RawKeyValue[] = [
+      ["key", "value"],
+      [
+        "key",
+        {
+          key1: {
+            key2: {
+              key3: "value3",
+            },
+            key4: "value4",
+          },
+          key5: {
+            key6: "value6",
+          },
+        },
+      ],
+    ];
+
+    const res = flattenArray(input);
+    const exp = [
+      ["key", "value"],
+      ["key.key1.key2.key3", "value3"],
+      ["key.key1.key4", "value4"],
+      ["key.key5.key6", "value6"]
+    ];
+
+    assert.deepEqual(res, exp);
+  });
+
   it("should flatten multiple times", () => {
     const input: RawKeyValue[] = [
       ["key", "value"],
